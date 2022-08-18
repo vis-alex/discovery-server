98d39e576a4e8bf3b66a69eea986c0e8b1a0bed2
vis-alex <usenkoshurik@mail.ru>
2022-08-18 11:29:15 +0300

    Add gateway

---
+++ src/test/java/com/alex/vis/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.vis.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
